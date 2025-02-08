9807006be41b9c0e482e02d074acec41079120ff
Mananalik <mananmalik556e@gmail.com>
2025-02-08 23:08:01 +0530

    created a router deleteTask

--- backend/src/controllers/task/taskController.js
+++ backend/src/controllers/task/taskController.js
@@ -66,3 +66,64 @@ try{
 }
 
 });
+export const updateTask = asyncHandler(async (req, res) => {
+    try {
+      const userId = req.user._id;
+  
+      const { id } = req.params;
+      const { title, description, dueDate, priority, status, completed } =
+        req.body;
+  
+      if (!id) {
+        res.status(400).json({ message: "Please provide a task id" });
+      }
+  
+      const task = await TaskModel.findById(id);
+  
+      if (!task) {
+        res.status(404).json({ message: "Task not found!" });
+      }
+  
+      // check if the user is the owner of the task
+      if (!task.user.equals(userId)) {
+        res.status(401).json({ message: "Not authorized!" });
+      }
+  
+      // update the task with the new data if provided or keep the old data
+      task.title = title || task.title;
+      task.description = description || task.description;
+      task.dueDate = dueDate || task.dueDate;
+      task.priority = priority || task.priority;
+      task.status = status || task.status;
+      task.completed = completed || task.completed;
+  
+      await task.save();
+  
+      return res.status(200).json(task);
+    } catch (error) {
+      console.log("Error in updateTask: ", error.message);
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+  // delete a task
+  export const deleteTask = asyncHandler(async (req,res)=>{
+    try{
+        const userId = req.user._id;
+        const {id} = req.params;
+        const task = await TaskModel.findById(id);
+        if(!task){
+            res.status(400).json({message:"Task not found"});
+        }
+    // check if the user is the owner of the task
+    if (!task.user.equals(userId)) {
+        res.status(401).json({ message: "Not authorized!" });
+      }
+      await TaskModel.findByIdAndDelete(id);
+      return res.status(200).json({message:"Task deleted successfully!"});
+    }catch(error){
+        console.log("Error in deleteTask: ", error.message);
+        res.status(500).json({message:error.message});
+    }
+  });
+  
