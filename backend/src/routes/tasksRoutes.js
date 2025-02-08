a81c0250830d7802e765d93fb03210e7f1116503
Mananalik <mananmalik556e@gmail.com>
2025-02-08 22:51:40 +0530

    created a updateTask router

--- backend/src/routes/tasksRoutes.js
+++ backend/src/routes/tasksRoutes.js
@@ -2,13 +2,11 @@ import express from "express";
 import {
     createTask,
     getTasks,
-    getTask,
-    updateTask
+    getTask
 } from "../controllers/task/taskController.js";
 import {protect} from "../middleware/authMiddleware.js";
 const router  = express.Router();
 router.post("/task/create",protect,createTask);
 router.get("/tasks",protect,getTasks);
 router.get("/task/:id",protect,getTask)
-router.patch("/task/:id",protect, updateTask);
 export default router;
