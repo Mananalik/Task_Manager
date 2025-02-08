1b2f4d8fabcae85202a58ead4620fed87a454dde
Mananalik <mananmalik556e@gmail.com>
2025-02-08 22:20:52 +0530

    created a router to get a single task

--- backend/src/controllers/task/taskController.js
+++ backend/src/controllers/task/taskController.js
@@ -27,7 +27,6 @@ export const createTask = asyncHandler(async(req,res)=>{
 });
 
 // get all the tasks
-//.tdtfhfhfyjfrj
 export const getTasks = asyncHandler(async(req,res)=>{
 try{
     const userId = req.user._id;
@@ -47,5 +46,23 @@ try{
 
 // get a task
 export const getTask = asyncHandler(async (req,res)=>{
+try{
+    const userId = req.user._id;
+    const {id} = req.params;
+    if(!id){
+        res.status(400).json({message:"Please provide a task id"});
+    }
+    const task = await TaskModel.findById(id);
+    if(!task){
+        res.status(400).json({message:"Task not found"});
+    }
+    if(!task.user.equals(userId)){
+        res.status(401).json({message:"Not authorized"});
+    }
+    res.status(200).json(task);
+}catch (error){
+    console.log("Error in getTask: ",error.message);
+    res.status(500).json({message:error.message});
+}
 
 });
--- backend/src/routes/tasksRoutes.js
+++ backend/src/routes/tasksRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import {
     createTask,
-    getTasks
+    getTasks,
+    getTask
 } from "../controllers/task/taskController.js";
 import {protect} from "../middleware/authMiddleware.js";
 const router  = express.Router();
 router.post("/task/create",protect,createTask);
 router.get("/tasks",protect,getTasks);
+router.get("/task/:id",protect,getTask)
 export default router;
