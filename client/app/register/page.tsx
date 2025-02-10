c12828d4a2a5d232d0db9eb088ce8d677cc0f737
Mananalik <mananmalik556e@gmail.com>
2025-02-10 23:32:22 +0530

    created a component RegisterForm

---
+++ client/app/register/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RegisterForm from '../components/auth/RegisterForm/RegisterForm';
+export default function page() {
+  return (
+    <div className="auth-page w-full h-full flex justify-center items-center">
+        <RegisterForm /></div>
+  );
+}
