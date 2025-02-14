c8ba6e0bb50e409968fac1f8353c55ed564a449e
Mananalik <mananmalik556e@gmail.com>
2025-02-14 18:20:08 +0530

    created a component forgot-password

---
+++ client/app/forgot-password/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ForgotPassWordForm from '../components/auth/ForgotPasswordForm/ForgotPasswordForm'
+export default function page() {
+  return (
+    <div className="auth-page w-full h-full flex justify-center items-center">
+        <ForgotPassWordForm/>
+    </div>
+  );
+}
