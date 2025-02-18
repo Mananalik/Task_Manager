37cce059f4b8c123ac43d069dcb158e8c5136a36
Mananalik <mananmalik556e@gmail.com>
2025-02-18 18:20:19 +0530

    worked on radial chart

--- client/providers/SideBarProvider.tsx
+++ client/providers/SideBarProvider.tsx
@@ -1,8 +1,11 @@
+"use client"
 import React from 'react'
-
+import Sidebar from '@/app/components/auth/Sidebar/Sidebar';
+import { useUserContext } from '@/context/userContext'
 function SideBarProvider() {
+  const userId = useUserContext().user._id;
   return (
-    <div>SideBarProvider</div>
+    <div>{userId && <Sidebar/>}</div>
   )
 }
 
