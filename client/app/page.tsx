bf78dde713ff0e44a09b875e5206847c5cc2803a
Mananalik <mananmalik556e@gmail.com>
2025-02-20 07:12:27 +0530

    completed home page with animations

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,22 @@
 "use client";
+import { useEffect } from "react";
 import {useTasks} from "@/context/taskContext";
 import useRedirect from "@/hooks/useUserRedirect";
 import { useState } from "react";
 import ChangePasswordForm from "./components/auth/changePasswordForm/ChangePasswordForm";
 import Filters from "./components/auth/Filters/Filters";
 import {Task} from "@/utils/types";
+import { filteredTasks } from "@/utils/utilities";
+import { motion } from "framer-motion";
+import { container, item } from "@/utils/animations";
+import TaskItem from "./components/auth/TaskItem/TaskItem";
 export default function Home() {
   useRedirect("/login");
   const {tasks, openModalForAdd, priority, setPriority} = useTasks();
+  const filtered = filteredTasks(tasks,priority);
+  useEffect(()=>{
+    setPriority("all");
+  },[])
   return (
    
     <main className="m-6 h-full">
@@ -17,6 +26,24 @@ export default function Home() {
           </h1>
           <Filters/>
         </div>
+        <motion.div
+         className="pb-[2rem] mt-6 grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-[1.5rem]"
+         variants={container}
+         initial="hidden"
+         animate="visible"
+         >
+          {filtered.map((task:Task,i:number)=>(
+            <TaskItem key={i}  task={task}/>
+          ))}
+          <motion.button
+          className="h-[16rem] w-full py-2 rounded-md text-lg font-medium text-gray-500 border-dashed border-2 border-gray-400
+          hover:bg-gray-300 hover:border-none transition duration-200 ease-in-out"
+          onClick={openModalForAdd}
+          variants={item}
+          >
+              Add New Task
+          </motion.button>
+         </motion.div>
     </main>
   );
 }
